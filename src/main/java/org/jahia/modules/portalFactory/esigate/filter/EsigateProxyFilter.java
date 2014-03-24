3d80e2ec8e0cefc838cd3a665bf3c34337cc1bbc
Kevan Jahanshahi <kevan@jahia.com>
2014-03-24 16:17:57 +0100

    esigate filter

---
+++ src/main/java/org/jahia/modules/portalFactory/esigate/filter/EsigateProxyFilter.java
@@ -0,0 +1,30 @@
+package org.jahia.modules.portalFactory.esigate.filter;
+
+import org.esigate.servlet.ProxyFilter;
+import org.jahia.bin.filters.AbstractServletFilter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by kevan on 24/03/14.
+ */
+public class EsigateProxyFilter extends AbstractServletFilter {
+    ProxyFilter proxyFilter;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        proxyFilter = new ProxyFilter();
+        proxyFilter.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        proxyFilter.doFilter(servletRequest, servletResponse, filterChain);
+    }
+
+    @Override
+    public void destroy() {
+        proxyFilter.destroy();
+    }
+}
